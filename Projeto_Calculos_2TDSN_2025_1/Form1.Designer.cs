c2cccde7bb475068cd4fb7f0e5b92eea860dbee4
LabInfo <LabInfo@TERREOA2M11>
2025-02-27 20:07:06 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto_Calculos_2TDSN_2025_1/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace Projeto_Calculos_2TDSN_2025_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblSinal = new System.Windows.Forms.Label();
+            this.lblTraço = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.txtN2 = new System.Windows.Forms.TextBox();
+            this.btnSomar = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.txtN1 = new System.Windows.Forms.TextBox();
+            this.btnSubtrair = new System.Windows.Forms.Button();
+            this.btnMultiplicar = new System.Windows.Forms.Button();
+            this.btnDividir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblSinal
+            // 
+            this.lblSinal.AutoSize = true;
+            this.lblSinal.BackColor = System.Drawing.Color.Transparent;
+            this.lblSinal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblSinal.Font = new System.Drawing.Font("Microsoft YaHei UI", 8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSinal.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblSinal.Location = new System.Drawing.Point(97, 142);
+            this.lblSinal.Name = "lblSinal";
+            this.lblSinal.Size = new System.Drawing.Size(20, 24);
+            this.lblSinal.TabIndex = 0;
+            this.lblSinal.Text = "?";
+            this.lblSinal.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblTraço
+            // 
+            this.lblTraço.AutoSize = true;
+            this.lblTraço.Font = new System.Drawing.Font("Microsoft YaHei UI", 8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTraço.Location = new System.Drawing.Point(105, 157);
+            this.lblTraço.Name = "lblTraço";
+            this.lblTraço.Size = new System.Drawing.Size(262, 22);
+            this.lblTraço.TabIndex = 1;
+            this.lblTraço.Text = "------------------------------------";
+            this.lblTraço.Click += new System.EventHandler(this.lblResultado_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft YaHei UI", 8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(191, 185);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(20, 24);
+            this.lblResultado.TabIndex = 2;
+            this.lblResultado.Text = "?";
+            this.lblResultado.Click += new System.EventHandler(this.blbResultado_Click);
+            // 
+            // txtN2
+            // 
+            this.txtN2.Font = new System.Drawing.Font("Microsoft YaHei UI", 8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtN2.Location = new System.Drawing.Point(123, 138);
+            this.txtN2.Name = "txtN2";
+            this.txtN2.Size = new System.Drawing.Size(153, 28);
+            this.txtN2.TabIndex = 6;
+            this.txtN2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtN2.TextChanged += new System.EventHandler(this.txtN2_TextChanged);
+            // 
+            // btnSomar
+            // 
+            this.btnSomar.Font = new System.Drawing.Font("Microsoft Tai Le", 8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSomar.Location = new System.Drawing.Point(504, 47);
+            this.btnSomar.Name = "btnSomar";
+            this.btnSomar.Size = new System.Drawing.Size(122, 35);
+            this.btnSomar.TabIndex = 7;
+            this.btnSomar.Text = "&Somar";
+            this.btnSomar.UseVisualStyleBackColor = true;
+            this.btnSomar.Click += new System.EventHandler(this.btnSomar_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Font = new System.Drawing.Font("Microsoft Tai Le", 8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.Location = new System.Drawing.Point(76, 249);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(122, 36);
+            this.btnLimpar.TabIndex = 8;
+            this.btnLimpar.Text = "&Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Font = new System.Drawing.Font("Microsoft Tai Le", 8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFechar.Location = new System.Drawing.Point(292, 248);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(122, 37);
+            this.btnFechar.TabIndex = 9;
+            this.btnFechar.Text = "&Fechar";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            // 
+            // txtN1
+            // 
+            this.txtN1.Font = new System.Drawing.Font("Microsoft YaHei UI", 8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtN1.Location = new System.Drawing.Point(123, 97);
+            this.txtN1.Name = "txtN1";
+            this.txtN1.Size = new System.Drawing.Size(153, 28);
+            this.txtN1.TabIndex = 10;
+            this.txtN1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtN1.TextChanged += new System.EventHandler(this.txtN1_TextChanged);
+            // 
+            // btnSubtrair
+            // 
+            this.btnSubtrair.Font = new System.Drawing.Font("Microsoft Tai Le", 8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubtrair.Location = new System.Drawing.Point(504, 94);
+            this.btnSubtrair.Name = "btnSubtrair";
+            this.btnSubtrair.Size = new System.Drawing.Size(122, 31);
+            this.btnSubtrair.TabIndex = 11;
+            this.btnSubtrair.Text = "&Subtrair";
+            this.btnSubtrair.UseVisualStyleBackColor = true;
+            this.btnSubtrair.Click += new System.EventHandler(this.btnSubtrair_Click_1);
+            this.btnSubtrair.Paint += new System.Windows.Forms.PaintEventHandler(this.btnSubtrair_Paint);
+            // 
+            // btnMultiplicar
+            // 
+            this.btnMultiplicar.Font = new System.Drawing.Font("Microsoft Tai Le", 8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMultiplicar.Location = new System.Drawing.Point(504, 148);
+            this.btnMultiplicar.Name = "btnMultiplicar";
+            this.btnMultiplicar.Size = new System.Drawing.Size(122, 40);
+            this.btnMultiplicar.TabIndex = 12;
+            this.btnMultiplicar.Text = "&Multiplicar";
+            this.btnMultiplicar.UseVisualStyleBackColor = true;
+            this.btnMultiplicar.Click += new System.EventHandler(this.btnMultiplicar_Click);
+            // 
+            // btnDividir
+            // 
+            this.btnDividir.Font = new System.Drawing.Font("Microsoft Tai Le", 8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDividir.Location = new System.Drawing.Point(504, 194);
+            this.btnDividir.Name = "btnDividir";
+            this.btnDividir.Size = new System.Drawing.Size(122, 37);
+            this.btnDividir.TabIndex = 13;
+            this.btnDividir.Text = "Dividir";
+            this.btnDividir.UseVisualStyleBackColor = true;
+            this.btnDividir.Click += new System.EventHandler(this.btnDividir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SteelBlue;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(740, 424);
+            this.Controls.Add(this.btnDividir);
+            this.Controls.Add(this.btnMultiplicar);
+            this.Controls.Add(this.btnSubtrair);
+            this.Controls.Add(this.txtN1);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnSomar);
+            this.Controls.Add(this.txtN2);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.lblTraço);
+            this.Controls.Add(this.lblSinal);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSinal;
+        private System.Windows.Forms.Label lblTraço;
+        private System.Windows.Forms.TextBox txtN2;
+        private System.Windows.Forms.Button btnSomar;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.TextBox txtN1;
+        public System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Button btnSubtrair;
+        private System.Windows.Forms.Button btnMultiplicar;
+        private System.Windows.Forms.Button btnDividir;
+    }
+}
+
---
+++ Projeto_Calculos_2TDSN_2025_1/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projeto_Calculos_2TDSN_2025_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void btnSomar_Click(object sender, EventArgs e)
+        {
+            lblSinal.Text = "+";
+            double a, b;
+            try
+            {
+                a = double.Parse(txtN1.Text);
+                b = double.Parse(txtN2.Text);
+
+                lblResultado.Text = (a + b).ToString();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show(this, "Favor apenas números", "Erro!!!");
+            }
+        }
+           
+
+        private void txtN1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtN2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblResultado_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void blbResultado_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSubtrair_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtN1.Clear();
+            txtN2.Clear();
+            lblSinal.Text = "?";
+            lblResultado.Text = "?";   
+            txtN1.Focus();
+        }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnSubtrair_Click_1(object sender, EventArgs e)
+        {
+            lblSinal.Text = "-";
+            double a, b;
+            try
+            {
+                a = double.Parse(txtN1.Text);
+                b = double.Parse(txtN2.Text);
+
+                lblResultado.Text = (a - b).ToString();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show(this, "Favor apenas números", "Erro!!!");
+            }
+        }
+
+        private void btnMultiplicar_Click(object sender, EventArgs e)
+        {
+            lblSinal.Text = "x";
+            double a, b;
+            try
+            {
+                a = double.Parse(txtN1.Text);
+                b = double.Parse(txtN2.Text);
+
+                lblResultado.Text = (a * b).ToString();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show(this, "Favor apenas números", "Erro!!!");
+            }
+        }
+
+        private void btnDividir_Click(object sender, EventArgs e)
+        {
+            lblSinal.Text = "%";
+            decimal a, b;
+            try
+            {
+                a = decimal.Parse(txtN1.Text);
+                b = decimal.Parse(txtN2.Text);
+
+                lblResultado.Text = (a / b).ToString();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show(this, "Favor apenas números", "Erro!!!");
+            }
+            catch (DivideByZeroException)
+            {
+                MessageBox.Show(this, "Impossível divir zero");
+            }
+            {
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSubtrair_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
